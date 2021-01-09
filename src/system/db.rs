b645b4ed2eabfdb89fda9bc3a397b798735036fe
stanisdev <javascript.nodejs.developer@gmail.com>
2021-01-09 14:40:26 +0600

    Altering the way of determining the type of a query

--- src/system/db.rs
+++ src/system/db.rs
@@ -3,31 +3,45 @@ use file::File;
 
 pub struct Db {
   query: String,
-  // file: File,
+  file: File,
 }
 
 impl Db {
+  /**
+   * Create a new instance of the struct
+   */
   pub fn new(query: String) -> Self {
     Db {
       query,
-      // file: File::new(),
+      file: File::new(),
     }
   }
 
+  /**
+   * Execute a query
+   */
   pub fn execute(&self) {
-    // @todo: split by regexp
-    let params: Vec<&str> = self.query.as_str().split(" ").collect();
-    let data = params[1].to_string();
+    match self.query.find(char::is_whitespace) {
+      Some(index) => {
+        let command = &self.query[..index];
+        let data = &self.query[index + 1..];
 
-    match params[0].to_lowercase().as_str() {
-      "find" => self.find(data),
-      _ => (),
+        match command.to_lowercase().as_str() {
+          "find" => self.find(data),
+          "save" => self.save(),
+          _ => (),
+        }
+      },
+      None => panic!("unrecognizable query"),
     }
   }
 }
 
 impl Db {
-  fn find(&self, value: String) {
+  /**
+   * Find value by a key
+   */
+  fn find(&self, value: &str) {
     let file = File::new();
     for line in file {
       println!("{}", line);
